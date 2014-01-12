d6291a6dd75ed10fca4597ee0577c05ed5be43e9
Javier Figueroa <figueroa7@gmail.com>
2014-01-12 18:36:05 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,6 @@ RailsStripeMembershipSaas::Application.routes.draw do
   get '/features', :to => 'home#features'
   get '/examples', :to => 'home#examples'
   get '/about', :to => 'home#about'
-#   
-  # authenticated :user do
-    # root :to => 'content#index', as: :authenticated_root
-  # end
-  # root :to => 'devise/sessions#new'
-  
   devise_for :users, :controllers => { :registrations => 'registrations', :sessions => "sessions" }
   devise_scope :user do
     put 'update_plan', :to => 'registrations#update_plan'
@@ -23,23 +17,13 @@ RailsStripeMembershipSaas::Application.routes.draw do
     root :to => 'devise/sessions#new'
   end
 
-  resources :timecards 
-  resources :projects  
-  resources :users
   
   get 'users/:in_date/:out_date', :to => 'users#date' 
   get 'projects/:in_date/:out_date', :to => 'projects#date' 
-  get 'timecards/today', :to => 'timecards#today' 
+  get 'timecards/today.json', :to => 'timecards#today' 
   get 'timecards/:in_date/:out_date', :to => 'timecards#date_and_user_id' 
   
-  # scope :constraints => lambda {|r| r.env["warden"].authenticate? } do
-    # all my logged in routes
-    #backbone endpoints
-    get 'users/:in_date/:out_date', :to => 'content#index'
-    get 'projects/:in_date/:out_date', :to => 'content#index'
-    get 'timecards/:in_date/:out_date/project/:project_id', :to => 'content#index'
-    get 'timecards/:in_date/:out_date/project/:project_id/:timecard_id', :to => 'content#index'
-    get 'timecards/:in_date/:out_date/user/:user_id', :to => 'content#index'
-    get 'timecards/:in_date/:out_date/user/:user_id/:timecard_id', :to => 'content#index'
-  # end
+  resources :timecards 
+  resources :projects  
+  resources :users
 end
