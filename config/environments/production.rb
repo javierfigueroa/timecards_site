4e3a750da122c13dca15f8a8082ca5baadbb7345
Javier Figueroa <figueroa7@gmail.com>
2014-07-07 23:43:22 -0400

    added https to the mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ RailsStripeMembershipSaas::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'timecards.io' }
+  config.action_mailer.default_url_options = { :host => 'timecards.io', :protocol => 'https' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
